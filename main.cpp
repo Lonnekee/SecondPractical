cedfefeb5e9b5536ae7b51b457129596fb2b1379
Lonnekee <36303116+Lonnekee@users.noreply.github.com>
2019-12-20 12:19:25 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -41,33 +41,28 @@ int main() {
      * - 819199: int maximumWaiting = 3, numberOfFloors = 5, capacity = 4, numberOfLifts = 1;
      */
 
-//      To calculate the number of states:
-//    int maximumWaiting = 4, numberOfFloors = 3, capacity = 2, numberOfLifts = 2;
-//    int numberOfStates = pow(maximumWaiting+1,numberOfFloors) * pow(((capacity+1) * numberOfFloors * pow(2, numberOfFloors)), numberOfLifts);
-//    cout << numberOfStates << endl;
-
-//    Elevator e;
-//    Floor f0, f1, f2, f3, f4;
-//    // Each floor should have a different random distribution for adding waiting people
-//    int maxEpochs = 1000;
-//
-//    for (int epoch = 1; epoch < maxEpochs; epoch++) {
-//        // Lifts get rewards
-//
-//
-//        // Lifts choose an action.
-//
-//        // Update state: lift's action has been performed (go to floor, some people step out),
-//        // randomly add waiting people on each floor.
-//    }
-
-//    int numberOfFloors = 5;
-//    Elevator elevatorOne;
-//    Floor *floorArray = new Floor[numberOfFloors];
-//    printState(numberOfFloors,floorArray,elevatorOne);
-//    delete(floorArray);
+//  To calculate the number of states:
+    int maximumWaiting = 4, numberOfFloors = 3, capacity = 2, numberOfLifts = 1;
+    int numberOfStates = pow(maximumWaiting+1,numberOfFloors) * pow(((capacity+1) * numberOfFloors * pow(2, numberOfFloors)), numberOfLifts);
+    cout << "Number of states: " << numberOfStates << endl;
 
     State s;
+    // Each floor should have a different random distribution for adding waiting people
+    int maxEpochs = 1000;
+
+    for (int epoch = 1; epoch < maxEpochs; epoch++) {
+        // Lifts get rewards
+        s.giveRewards();
+
+        // Lifts choose an action.
+        s.selectActions();
+
+        // Update state: lift's action has been performed (go to floor, some people step out),
+        // randomly add waiting people on each floor.
+        s.updateState();
+    }
+
     printState(5,s.getFloors(),s.getElevators()[0]);
+
     return 0;
 }
